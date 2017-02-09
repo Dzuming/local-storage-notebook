ae8c7803c481e3a06193afd45d36b5d5a3ddae66
POLISZAD <dawid.poliszak@contractors.roche.com>
2017-02-09 15:43:23 +0100

    Add refresh

--- src/app/note-list/note-list.component.ts
+++ src/app/note-list/note-list.component.ts
@@ -1,11 +1,11 @@
-//On click = take object name
+
 //TODO - resetowanie wyboru;
-//TODO - Odświeżanie;
 //TODO - Przy pustym jsonie wyskakuje formError
-//TODO - Problem z wyborem rodzica
 //TODO - Usuwanie z listy
 //TODO - rozwijanie listy po kliknieciu na przycisk
-
+//TODO - walidacja danych
+//TODO - Bug z usunieciem ostatniego
+//Show to another function
 import { Component, OnInit, ElementRef, Renderer, AfterViewInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { LocalStorageService } from '../shared/local-storage.service';
@@ -33,13 +33,12 @@ export class NoteListComponent implements OnInit, AfterViewInit {
   ngOnInit() {
     this.buildForm();
     this.show(JSON.parse(this.getData()));
-    let el = document.querySelector('app-note-list')
-    el.insertAdjacentHTML('afterbegin', this.html.join(''))
+    this.updateList()
   }
   ngAfterViewInit() {
 
     this.clickableElementToFind()
-
+    this.delete()
   }
   public buildForm(): void {
     this.noteForm = this.formBuilder.group({
@@ -57,34 +56,15 @@ export class NoteListComponent implements OnInit, AfterViewInit {
   }
   save() {
     let value: Array<Object> = JSON.parse(this.getData()) || [];
-    // value.map((element)=> {
-
-    //  this.localStorageService.saveItem(this.noteObject, value)
-    //   if (Object.keys(element[0].children).length !== 0 && element[0].children.constructor === Object) {
-    //     console.log(element[0].children)
-    //   }
-
-    // })
-    // for(var prop in value[0]["test5"]) {
-    //   if( value[0].hasOwnProperty(prop) && typeof value[0][prop] === 'object' ) {
-    //   }
-    //   console.log(prop)
-    // }
-    // value[0][0].children.push([{
-    //   "name": this.noteForm.value.Name,
-    //   "children": {},
-    //   "Description": this.noteForm.value.Description
-    // }])
+    this.removeList();
     this.onSave(value);
 
-    // let ul = document.querySelector('ul')
-    //   ul.parentNode.removeChild(ul);
-    //   if (ul.innerHTML !== "") {
-    //     this.show(JSON.parse(this.getData()))
-    //   }
 
   }
   onSave(value) {
+    if (this.html) {
+      this.html = [];
+    }
     if (!this.nameOnClick) {
       value.push([{
         "name": this.noteForm.value.Name,
@@ -93,7 +73,7 @@ export class NoteListComponent implements OnInit, AfterViewInit {
       }])
     }
     value.map((val, index) => {
-      if (val[0].name === this.nameOnClick) {
+      if (val[0].name === this.nameOnClick && this.nameOnClick !== "") {
         val[0].children.push([{
           "name": this.noteForm.value.Name,
           "children": [],
@@ -103,8 +83,37 @@ export class NoteListComponent implements OnInit, AfterViewInit {
         this.onSave(val[0].children)
       }
     })
-    console.log(value)
+    this.localStorageService.saveItem(this.noteObject, value);
+    this.show(JSON.parse(this.getData()));
+    this.updateList()
+  }
+  delete() {
+    let remove = this.elementRef.nativeElement.querySelectorAll('.delete');
+    let click = Observable.fromEvent(remove, 'click');
+    let subscription = click.subscribe(
+      (e: any) => {
+        this.nameOnClick = e.currentTarget.previousSibling.innerHTML.split("<ul>")[0];
+        this.onDelete(JSON.parse(this.getData()))
+      })
+  }
+  onDelete(value) {
+    if (this.html) {
+      this.html = [];
+    }
+    this.removeList();
+    if (!this.nameOnClick) {
+      value.splice(1, 1);
+    }
+    value.map((val, index) => {
+      if (val[0].name === this.nameOnClick) {
+        val.splice(index, 1);
+      } else if (val[0].children[0] && val[0].name !== this.nameOnClick) {
+        this.onDelete(val[0].children)
+      }
+    })
     this.localStorageService.saveItem(this.noteObject, value)
+    this.updateList();
+    this.delete();
   }
   find(element) {
     element.map((val) => {
@@ -116,29 +125,34 @@ export class NoteListComponent implements OnInit, AfterViewInit {
     })
   }
   clickableElementToFind() {
-    let li = this.elementRef.nativeElement.querySelectorAll('li');
-    let clickFlag: boolean = false;
-    let click = Observable.fromEvent(li, 'click');
+    let span = this.elementRef.nativeElement.querySelectorAll('span');
+    let click = Observable.fromEvent(span, 'click');
     let subscription = click.subscribe(
       (e: any) => {
-        if (!clickFlag) {
-          this.nameOnClick = e.currentTarget.innerHTML.split("<ul>")[0];
-          console.log(this.nameOnClick.split("<ul>")[0])
-          this.find(JSON.parse(this.getData()))
-          return clickFlag = true;
-        } else {
-
-
-          return clickFlag = false;
-        }
-
-      })
+        this.nameOnClick = e.currentTarget.innerHTML.split("<ul>")[0];
+        this.find(JSON.parse(this.getData()));
+      }
+    )
 
   }
+  updateList() {
+    let el = document.querySelector('app-note-list')
+    el.insertAdjacentHTML('afterbegin', this.html.join(''))
+  }
+  removeList() {
+    let ul = document.querySelector('ul')
+    if (ul) {
+      ul.parentNode.removeChild(ul);
+    }
+  }
   show(element) {
+    if (!element) {
+      return
+    }
     this.html.push('<ul>');
     element.map((val) => {
-      this.html.push('<li>' + val[0].name);
+      this.html.push('<li><i class="unroll">S</i><span>' + val[0].name);
+      this.html.push('</span><i class="delete">X</i>');
       if (val[0].children[0]) {
         this.show(val[0].children)
       }
